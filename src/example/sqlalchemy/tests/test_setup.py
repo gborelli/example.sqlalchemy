1135beb2f0dd0996ef1ff4f707073ad76e3a9ed7
Giorgio Borelli <casaborelli@gmail.com>
2013-07-22 18:53:21 +0200

    fix tests

--- src/example/sqlalchemy/tests/test_setup.py
+++ src/example/sqlalchemy/tests/test_setup.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
 from zope.component import getUtility
-from ..testing import INTEGRATION_LAYER
+from ..testing import FUNCTIONAL_LAYER
 from ..model import Session
 from ..model import Message
 
 
 class TestSetup(unittest.TestCase):
-    layer = INTEGRATION_LAYER
+    layer = FUNCTIONAL_LAYER
 
     def setUp(self):
         self.portal = self.layer['portal']
+        self.request = self.layer['request']
 
     def test_db(self):
         # semplice test che verifica i dati salvati all'installazione
-        # e spiega l'uso della sessione
+        # e visualizza l'uso della sessione
         session = Session()
         self.assertEqual(session.query(Message).count(), 5)
 
     def test_view(self):
+        # questo attributo viene impostato dal publisher
+        self.portal.REQUEST.path = []
         view = self.portal.restrictedTraverse('@@messages')
+        self.assertEqual(len(view.results()),  5)
 
-        res = [i for i in view.results()]
-        self.assertEqual(res,  5)
-        import pdb; pdb.set_trace( )
+        self.portal.REQUEST.path = ['2012']
+        view = self.portal.restrictedTraverse('@@messages')
+        self.assertEqual(len(view.results()),  2)
+
+        self.portal.REQUEST.path = ['1', '2012']
+        view = self.portal.restrictedTraverse('@@messages')
+        item = view.results()
+        self.assertEqual(item['title'], u'test 1')
